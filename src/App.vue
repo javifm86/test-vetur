21a2d7712f291bd5636fba1aa3cb76484da5241f
Javier Fernández <jfmarquez@renta4.es>
2021-07-16 15:39:57 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid sm:grid-cols-2 lg:grid-cols-3 auto-rows-auto sm:auto-rows-fr bg-white rounded-lg overflow-hidden border border-gray-200" ></div>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
